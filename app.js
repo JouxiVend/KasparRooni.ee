40859b2665abcdb174279d74d4955a067cbdab75
n0ll0 <101214789+n0ll0@users.noreply.github.com>
2023-10-06 15:42:55 +0300

    merged bin/www and app.js

--- app.js
+++ app.js
@@ -1,49 +1,134 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const fs = require('fs');
-const indexRouter = require('./routes/index');
-const adminRouter = require('./routes/admin');
-const sass = require('sass');
+/**
+ * Module dependencies.
+ */
+
+// var debug = require('debug')('kasparrooni.ee:server');
+var http = require("http");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const fs = require("fs");
+const indexRouter = require("./routes/index");
+const adminRouter = require("./routes/admin");
+const sass = require("sass");
+
+const port = normalizePort(process.env.PORT || "3000");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+app.set("port", port);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 /**
  * Sass compiling
  */
-for (let e of fs.readdirSync(path.resolve(`${__dirname}/sass`), (err, files) => files.filter((e) => path.extname(e).toLowerCase() === '.sass'))) {
+for (let e of fs.readdirSync(path.resolve(`${__dirname}/sass`), (err, files) =>
+  files.filter((e) => path.extname(e).toLowerCase() === ".sass")
+)) {
   let result = sass.compile(`${__dirname}/sass/${e}`);
   fs.writeFileSync(`${__dirname}/public/stylesheets/${e.slice(0, -4)}css`, result.css);
 }
 /** */
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.use('/admin', adminRouter);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", indexRouter);
+app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-module.exports = app;
+/**
+ * Get port from environment and store in Express.
+ */
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  console.log("listening on ", bind);
+  console.log("http://localhost:" + addr.port);
+}
